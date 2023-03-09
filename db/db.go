dkorunic/e-dnevnik-bot

commit 76985a44f8630b8d8ffb75f4d33c0df6c073b8e4

When multiclass student has been detected, append class names to subjects

scrape/helpers.go
@@ -42,7 +42,7 @@ const (
 
 // parseGrades extracts grades per subject from raw string (grade scrape response body) and grade descriptions,
 // constructs grade messages and sends them a message channel, optionally returning an error.
-func parseGrades(ch chan<- msgtypes.Message, username, rawGrades string) error {
+func parseGrades(ch chan<- msgtypes.Message, username, rawGrades string, multiClass bool, className string) error {
 	doc, err := goquery.NewDocumentFromReader(strings.NewReader(rawGrades))
 	if err != nil {
 		return err
@@ -83,6 +83,11 @@ func parseGrades(ch chan<- msgtypes.Message, username, rawGrades string) error {
 							spans = append(spans, txt)
 						})
 
+					// if multiclass, append class name to subject
+					if multiClass {
+						subject = strings.Join([]string{subject, className}, " / ")
+					}
+
 					// once we have a single grade with all required fields, send it through the channel
 					ch <- msgtypes.Message{
 						Username:     username,
@@ -112,7 +117,7 @@ func cleanEventDescription(summary string) string {
 
 // parseEvents processes Events array, emitting a single exam message for each event, optionally returning an
 // error.
-func parseEvents(ch chan<- msgtypes.Message, username string, events fetch.Events) error {
+func parseEvents(ch chan<- msgtypes.Message, username string, events fetch.Events, multiClass bool, className string) error {
 	if len(events) == 0 {
 		logger.Info().Msgf("No scheduled exams for user %v", username)
 	}
@@ -121,6 +126,11 @@ func parseEvents(ch chan<- msgtypes.Message, username string, events fetch.Event
 		subject := cleanEventDescription(ev.Summary)
 		description := cleanEventDescription(ev.Description)
 		timestamp := ev.Start.Format(TimeFormat)
+
+		// if multiclass, append class name to subject
+		if multiClass {
+			subject = strings.Join([]string{subject, className}, " / ")
+		}
 
 		// send each event through channel
 		ch <- msgtypes.Message{

fetch/helpers.go
@@ -275,7 +275,8 @@ func (c *Client) doClassAction(classID string) error {
 		return fmt.Errorf("%w: %v", ErrUnexpectedStatus, resp.StatusCode)
 	}
 
-	_, err = io.Copy(io.Discard, resp.Body)
+	// drain rest of the body
+	io.Copy(io.Discard, resp.Body) //nolint:errcheck
 
 	return err
 }

scrape/scrape.go
@@ -80,13 +80,21 @@ func GetGradesAndEvents(ctx context.Context, ch chan<- msgtypes.Message, usernam
 			return err
 		}
 
-		logger.Debug().Msgf("Found active classes for user %v: %+v", username, classes)
+		multiClass := len(classes) > 1
+
+		if multiClass {
+			logger.Debug().Msgf("Found multiple active classes for user %v: %+v", username, classes)
+		} else {
+			logger.Debug().Msgf("Found active class for user %v: %+v", username, classes)
+		}
 
 		// iterate all active classes
 		for _, c := range classes {
-			c := c
+			cID := c.ID
+			cName := c.Name
 
-			logger.Debug().Msgf("Fetching grades/calendar for user: %v: class %v", username, c.Name)
+			logger.Debug().Msgf("Fetching grades and calendar events for user %v, class %v, class ID %v", username,
+				cName, cID)
 
 			// fetch subjects/grades/exams
 			var rawGrades string
@@ -94,7 +102,7 @@ func GetGradesAndEvents(ctx context.Context, ch chan<- msgtypes.Message, usernam
 			err = retry.Do(
 				func() error {
 					var err error
-					rawGrades, events, err = client.GetClassEvents(c.ID)
+					rawGrades, events, err = client.GetClassEvents(cID)
 
 					return err
 				},
@@ -106,13 +114,13 @@ func GetGradesAndEvents(ctx context.Context, ch chan<- msgtypes.Message, usernam
 			}
 
 			// parse all subjects and corresponding grades
-			err = parseGrades(ch, username, rawGrades)
+			err = parseGrades(ch, username, rawGrades, multiClass, cName)
 			if err != nil {
 				return err
 			}
 
 			// parse all exam events
-			err = parseEvents(ch, username, events)
+			err = parseEvents(ch, username, events, multiClass, cName)
 			if err != nil {
 				return err
 			}

commit 22ce309d08eb84f6d85305ebea99116fb8266709

Resolve multiclass subject suffix loop bug caused by 76985a4 Minor lint fixes

db/db.go
@@ -55,10 +55,11 @@ func New(filePath string) (*Edb, error) {
 	opts := badger.DefaultOptions(filePath)
 
 	// adapt for low memory environment
+	//nolint:gomnd
 	if strconv.IntSize == 32 {
 		logger.Info().Msg("Detected 32-bit environment, tuning DB for lower memory usage")
 
-		opts.ValueLogFileSize = 1 << 20
+		opts.ValueLogFileSize = 1 << 20 //nolint:gomnd
 	}
 
 	db, err := badger.Open(opts.WithLogger(nil)) // disable Badger verbose logging

fetch/events.go
@@ -37,6 +37,7 @@ const (
 func (e *Events) ConsumeICal(c *goics.Calendar, err error) error {
 	for _, el := range c.Events {
 		node := el.Data
+
 		dtstart, err := dateparse.ParseLocal(node[EventDateStart].Val)
 		if err != nil {
 			return err

format/plain.go
@@ -41,6 +41,8 @@ func PlainMsg(username, subject string, isExam bool, descriptions, grade []strin
 }
 
 // plainFormatGrades formats grade descriptions and values.
+//
+//nolint:interfacer
 func plainFormatGrades(sb *strings.Builder, descriptions, grade []string) {
 	for i := range grade {
 		// grade listing will print scraped corresponding descriptions
@@ -52,6 +54,8 @@ func plainFormatGrades(sb *strings.Builder, descriptions, grade []string) {
 }
 
 // PlainFormatSubject adds cleartext header containing prefix (event/grade), username and subject.
+//
+//nolint:interfacer
 func PlainFormatSubject(sb *strings.Builder, user, subject string, isExam bool) {
 	if isExam {
 		sb.WriteString(EventPrefix)

scrape/helpers.go
@@ -56,6 +56,11 @@ func parseGrades(ch chan<- msgtypes.Message, username, rawGrades string, multiCl
 				return
 			}
 
+			// if multiclass, append class name to subject
+			if multiClass {
+				subject = strings.Join([]string{subject, className}, " / ")
+			}
+
 			var descriptions []string
 			// row descriptions are in div with class "flex-table header" in each span
 			table.Find("div.flex-table.header > div.flex-row > span").
@@ -80,11 +85,6 @@ func parseGrades(ch chan<- msgtypes.Message, username, rawGrades string, multiCl
 							}
 							spans = append(spans, txt)
 						})
-
-					// if multiclass, append class name to subject
-					if multiClass {
-						subject = strings.Join([]string{subject, className}, " / ")
-					}
 
 					// once we have a single grade with all required fields, send it through the channel
 					ch <- msgtypes.Message{