ab9c4c2fb05dace96850549ee67e62f4e22f2ef2
Dinko Korunic <dinko.korunic@gmail.com>
2025-01-30 14:35:12 +0100

    Improve configuration checking

--- config.go
+++ config.go
@@ -22,11 +22,15 @@
 package main
 
 import (
+	stdmail "net/mail"
+	"regexp"
 	"slices"
+	"strconv"
 	"strings"
 
 	"github.com/BurntSushi/toml"
 	"github.com/dkorunic/e-dnevnik-bot/logger"
+	"go.mau.fi/whatsmeow/types"
 )
 
 // user struct holds a single AAI/SSO username.
@@ -93,6 +97,14 @@ type tomlConfig struct {
 	whatsAppEnabled bool     `toml:"whatsapp_enabled"`
 }
 
+var (
+	phoneRegex         = regexp.MustCompile(`^\+[1-9][0-9]{3,14}$`)
+	slackTokenRegex    = regexp.MustCompile(`^xox(?i:[abposr])-(?:\d+-)+[a-z0-9]+$`)
+	slackChatIDRegex   = regexp.MustCompile(`^[UWCGD][A-Z0-9]{8,}$|^\d{10}\.\d{6}$`)
+	discordTokenRegex  = regexp.MustCompile(`^[MNO][a-zA-Z\d_-]{23,25}\.[a-zA-Z\d_-]{6}\.[a-zA-Z\d_-]{27}$`)
+	telegramTokenRegex = regexp.MustCompile(`^\d{8,10}:[0-9A-Za-z_-]{35}$`)
+)
+
 // loadConfig attempts to load and decode configuration file in TOML format, doing a minimal sanity checking and
 // optionally returning an error.
 func loadConfig() (tomlConfig, error) {
@@ -101,26 +113,117 @@ func loadConfig() (tomlConfig, error) {
 		return config, err
 	}
 
-	if config.Discord.Token != "" && len(config.Discord.UserIDs) > 0 {
+	// check if users are defined
+	if len(config.User) == 0 {
+		logger.Fatal().Msg("Configuration error: No users defined")
+	}
+
+	// check if all users have username and password
+	for _, u := range config.User {
+		if u.Username == "" || u.Password == "" {
+			logger.Fatal().Msgf("Configuration error: user requires username and password: %q - %q",
+				u.Username, u.Password)
+		}
+
+		// check if username is in e-mail format
+		if !isValidMail(u.Username) {
+			logger.Fatal().Msgf("Configuration error: username not in e-mail format: %q", u.Username)
+		}
+
+		// check if username ends with @skole.hr
+		if !strings.HasSuffix(u.Username, "@skole.hr") {
+			logger.Warn().Msgf("Configuration issue: username not ending with @skole.hr: %q", u.Username)
+		}
+	}
+
+	// check if Discord token is defined
+	if config.Discord.Token != "" {
+		// check if token is valid
+		if !isValidDiscordToken(config.Discord.Token) {
+			logger.Fatal().Msgf("Configuration error: Discord token %q is not valid", config.Discord.Token)
+		}
+
+		// check if user IDs are defined
+		if len(config.Discord.UserIDs) == 0 {
+			logger.Fatal().Msg("Configuration error: Discord user IDs not defined")
+		}
+
+		// check if all user IDs are valid
+		for _, u := range config.Discord.UserIDs {
+			if !isValidID(u) {
+				logger.Fatal().Msgf("Configuration error: Discord user ID %q is not valid", u)
+			}
+		}
+
 		logger.Info().Msg("Configuration: Discord messenger enabled")
 
 		config.discordEnabled = true
 	}
 
-	if config.Telegram.Token != "" && len(config.Telegram.ChatIDs) > 0 {
+	// check if Telegram token is defined
+	if config.Telegram.Token != "" {
+		// check if token is valid
+		if !isValidTelegramToken(config.Telegram.Token) {
+			logger.Fatal().Msgf("Configuration error: Telegram token %q is not valid", config.Telegram.Token)
+		}
+
+		// check if user IDs are defined
+		if len(config.Telegram.ChatIDs) == 0 {
+			logger.Fatal().Msg("Configuration error: Telegram chat IDs not defined")
+		}
+
+		// check if all chat IDs are valid
+		for _, c := range config.Telegram.ChatIDs {
+			if !isValidID(c) {
+				logger.Fatal().Msgf("Configuration error: Telegram chat ID %q is not valid", c)
+			}
+		}
+
 		logger.Info().Msg("Configuration: Telegram messenger enabled")
 
 		config.telegramEnabled = true
 	}
 
-	if config.Slack.Token != "" && len(config.Slack.ChatIDs) > 0 {
+	// check if Slack token is defined
+	if config.Slack.Token != "" {
+		// check if token is valid
+		if !isValidSlackToken(config.Slack.Token) {
+			logger.Fatal().Msgf("Configuration error: Slack token %q is not valid", config.Slack.Token)
+		}
+
+		// check if user IDs are defined
+		if len(config.Slack.ChatIDs) == 0 {
+			logger.Fatal().Msg("Configuration error: Slack chat IDs not defined")
+		}
+
+		// check if all chat IDs are valid
+		for _, c := range config.Slack.ChatIDs {
+			if !isValidSlackChatID(c) {
+				logger.Fatal().Msgf("Configuration error: Slack chat ID %q is not valid", c)
+			}
+		}
+
 		logger.Info().Msg("Configuration: Slack messenger enabled")
 
 		config.slackEnabled = true
 	}
 
-	if config.Mail.Server != "" && config.Mail.From != "" && len(config.Mail.To) > 0 {
+	if config.Mail.Server != "" {
+		// check if any of e-mail destinations (TO) are defined
+		if len(config.Mail.To) == 0 {
+			logger.Fatal().Msg("Configuration error: no e-mail to addresses defined")
+		}
+
 		logger.Info().Msg("Configuration: e-mail messenger enabled (pending check during initialization)")
+
+		// no need to check e-mail FROM since it can be anything
+
+		// check if all destination e-mail addresses are valid
+		for _, t := range config.Mail.To {
+			if !isValidMail(t) {
+				logger.Fatal().Msgf("Configuration error: e-mail to %q is not in e-mail format", t)
+			}
+		}
 
 		config.mailEnabled = true
 	}
@@ -130,6 +233,19 @@ func loadConfig() (tomlConfig, error) {
 	}
 
 	if len(config.WhatsApp.UserIDs) > 0 || len(config.WhatsApp.Groups) > 0 {
+		// check if phone number is valid
+		if config.WhatsApp.PhoneNumber != "" && !isValidPhone(config.WhatsApp.PhoneNumber) {
+			logger.Fatal().Msgf("WhatsApp phone number %s is not in international format",
+				config.WhatsApp.PhoneNumber)
+		}
+
+		// check if all user IDs are valid
+		for _, u := range config.WhatsApp.UserIDs {
+			if !isValidWhatsAppJID(u) {
+				logger.Fatal().Msgf("Configuration error: WhatsApp user ID %q is not valid", u)
+			}
+		}
+
 		logger.Info().Msg("Configuration: Whatsapp messenger enabled (pending check during initialization)")
 
 		// sort group names for binary search in WhatsApp messenger
@@ -139,4 +255,100 @@ func loadConfig() (tomlConfig, error) {
 	}
 
 	return config, nil
+}
+
+// isValidPhone checks if the given phone number is in international format
+// (starting with "+", followed by 3-14 digits).
+//
+// Parameters:
+// - phone: the phone number to validate
+//
+// Returns:
+// - true if phone number is valid, false otherwise
+func isValidPhone(phone string) bool {
+	return phoneRegex.MatchString(phone)
+}
+
+// isValidMail checks if the given string is a valid e-mail address in the
+// format user@domain.tld.
+//
+// Parameters:
+// - mail: the e-mail address to validate
+//
+// Returns:
+// - true if the e-mail address is valid, false otherwise
+func isValidMail(mail string) bool {
+	_, err := stdmail.ParseAddress(mail)
+
+	return err == nil
+}
+
+// isValidID checks if the given string is a valid positive integer.
+//
+// Parameters:
+// - id: the string to validate
+//
+// Returns:
+// - true if the string is a valid positive integer, false otherwise
+func isValidID(id string) bool {
+	_, err := strconv.ParseUint(id, 10, 64)
+
+	return err == nil
+}
+
+// isValidSlackToken checks if the given string is a valid Slack token.
+//
+// Parameters:
+// - token: the Slack token to validate
+//
+// Returns:
+// - true if the Slack token is valid, false otherwise
+func isValidSlackToken(token string) bool {
+	return slackTokenRegex.MatchString(token)
+}
+
+// isValidSlackChatID checks if the given string is a valid Slack Chat ID.
+//
+// Parameters:
+// - id: the string to validate
+//
+// Returns:
+// - true if the string is a valid Slack Chat ID, false otherwise
+func isValidSlackChatID(id string) bool {
+	return slackChatIDRegex.MatchString(id)
+}
+
+// isValidDiscordToken checks if the given string is a valid Discord token.
+//
+// Parameters:
+// - token: the Discord token to validate
+//
+// Returns:
+// - true if the Discord token is valid, false otherwise
+func isValidDiscordToken(token string) bool {
+	return discordTokenRegex.MatchString(token)
+}
+
+// isValidTelegramToken checks if the given string is a valid Telegram token.
+//
+// Parameters:
+// - token: the Telegram token to validate
+//
+// Returns:
+// - true if the Telegram token is valid, false otherwise
+func isValidTelegramToken(token string) bool {
+	return telegramTokenRegex.MatchString(token)
+}
+
+// isValidWhatsAppJID checks if the given string is a valid WhatsApp group or user JID.
+//
+// Parameters:
+// - jid: the string to validate
+//
+// Returns:
+// - true if the string is a valid WhatsApp JID, false otherwise
+func isValidWhatsAppJID(jid string) bool {
+	_, err := types.ParseJID(jid)
+
+	return err == nil
 }
